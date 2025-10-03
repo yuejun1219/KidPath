3f6f7f16642d83a429076939d2c63c895fa3bfeb
helen439 <zhelen439@gmail.com>
2025-10-04 00:58:45 +1000

    delete unused api

--- Backend/src/routes/poi.js
+++ Backend/src/routes/poi.js
@@ -5,19 +5,12 @@ const router = express.Router();
 const { 
   findPOIsAlongRouteController, 
   findComfortPOIsAlongRouteController,
-  findPOIsInBboxController,
-  getPOICategoriesController,
-  findNearbyPOIsController,
   getRouteAmenitiesSummaryController
 } = require('../controllers/poiController');
-const { validateCoordinates, validateBbox } = require('../middleware/validation');
 
 // POI search endpoints
 router.post('/along-route', findPOIsAlongRouteController);
 router.post('/comfort-along-route', findComfortPOIsAlongRouteController);
-router.get('/in-bbox', validateBbox, findPOIsInBboxController);
-router.get('/categories', getPOICategoriesController);
-router.get('/nearby', validateCoordinates, findNearbyPOIsController);
 router.post('/route-amenities-summary', getRouteAmenitiesSummaryController);
 
 module.exports = router;
